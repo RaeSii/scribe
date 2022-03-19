a282c3d169a855497880e90a840b233b2441c7da
raesii <leisolo@gmail.com>
2022-03-19 15:56:05 -0700

    added html routes and api routes to server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-
+app.use(require("routes/api_routes")
+app.use(require("routes/html_routes")
 app.listen(PORT,function(){
     console.log("App is listening on the PORT")
 })
