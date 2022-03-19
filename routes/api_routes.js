aff628de314652449713917f172218d3109c2dd8
raesii <leisolo@gmail.com>
2022-03-19 15:38:27 -0700

    added api routes

---
+++ routes/api_routes.js
@@ -0,0 +1,32 @@
+const app = require('express').Router()
+const fs = require('fs')
+let db = require('../db/db.json');
+
+
+
+app.get("/api/notes",function(req,res){
+    db = JSON.parse(fs.readFileSync("db/db.json")) || []
+    console.log("Display all notes",db);
+    res.json(db)
+});
+
+
+app.post("/api/notes",function(req,res){
+    let newNote = {
+        id: Math.floor(Math.random() * 9999),
+        title: req.body.title,
+        text: req.body.text
+    }
+    db.push(newNote)
+   fs.writeFileSync("db/db.json",JSON.stringify(db),function(error){
+       if (err)throw error
+   })) 
+    console.log("Add New Note",db);
+    res.json(db)
+});
+
+
+
+
+
+module.exports = app;
