ecf775a2615d2fe510c27e73bfb5512eb22d4a9f
raesii <leisolo@gmail.com>
2022-03-19 16:02:00 -0700

    fixed error on missing api routes

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const { uptime } = require('process');
 const PORT = process.env.PORT||3000 ;
 const app = express()
+const api = require("./routes/api_routes");
+const html = require("./routes/html_routes")
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use(require("routes/api_routes")
-app.use(require("routes/html_routes")
+app.use(api)
+app.use(html)
+
+
 app.listen(PORT,function(){
     console.log("App is listening on the PORT")
 })
